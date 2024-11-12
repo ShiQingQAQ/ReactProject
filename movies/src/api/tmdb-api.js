b96160ac06493adb99ce42b534d7e9f7e8c2f5e5
ShiQingQAQ <20108872@mail.wit.ie>
2024-11-12 21:43:46 +0000

    add trending function

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -95,3 +95,19 @@ export const getMovie = (args) => {
     }
     return response.json();
   };
+  export const getTrendingMovies = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    ).then((response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error;
+    });
+  };
+  
