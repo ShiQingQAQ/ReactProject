ddde5401b573b20e093fe41c37b2d61ed9fb2e90
ShiQingQAQ <20108872@mail.wit.ie>
2024-11-21 19:22:21 +0000

    add new filtering options

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState }from "react";
 import { getMovies } from "../api/tmdb-api";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from 'react-query';
@@ -6,7 +6,7 @@ import Spinner from '../components/spinner';
 import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
 
 const HomePage = () => {
-
+  const [minRating, setMinRating] = useState(0);//新增状态
   const {  data, error, isLoading, isError }  = useQuery('discover', getMovies)
 
   if (isLoading) {
@@ -18,18 +18,38 @@ const HomePage = () => {
   }  
   const movies = data.results;
 
+   // 应用最低评分过滤
+  const filteredMovies = movies.filter(movie => movie.vote_average >= minRating);
+
   // Redundant, but necessary to avoid app crashing.
   const favorites = movies.filter(m => m.favorite)
   localStorage.setItem('favorites', JSON.stringify(favorites))
 
   return (
+    <>
+    <div>
+      {/* 新增的过滤选项 */}
+      <lable htmlFor="minRating">Minimum Rating:</lable>
+      <input
+        type="number"
+        id="minRating"
+        name="minRating"
+        min="0"
+        max="10"
+        step="0.1"
+        value={minRating}
+        onChange={(e) => setMinRating(e.target.value)}
+      />
+    </div>
+
     <PageTemplate
       title="Discover Movies"
-      movies={movies}
+      movies={filteredMovies} // 使用过滤后的电影列表
       action={(movie) => {
         return <AddToFavoritesIcon movie={movie} />
       }}
     />
+    </>
 );
 };
 export default HomePage;
